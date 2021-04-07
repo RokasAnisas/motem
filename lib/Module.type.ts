450ea3c38d7ff1c149db6496afc6c719b229e129
Rokas Anisas <rokas.anisas@gmail.com>
2021-04-07 13:55:01 +0300

    Create Module.type.ts

---
+++ lib/Module.type.ts
@@ -0,0 +1,14 @@
+interface Module {
+  add?: Array<{
+    directory: string;
+    files: Array<{
+      fileName: string;
+      template: string;
+    }>;
+  }>;
+  modify?: Array<{
+    file: string;
+    from: string;
+    to: string;
+  }>
+}
