545c901c3651179ed5cbda0f369ec0d51aa6bee1
Rokas Anisas <rokas.anisas@gmail.com>
2021-04-07 14:00:14 +0300

    Update Module.type.ts

--- lib/Module.type.ts
+++ lib/Module.type.ts
@@ -1,12 +1,28 @@
 interface Module {
   add?: Array<{
+    /**
+     * Relative path to the directory where files will be created
+     * (src/directory).
+     */
     directory: string;
     files: Array<{
+      /**
+       * [Filename].ts
+       * Use the keyword file name to specify casing:
+       * [filenName] - camel case
+       * [FileName] - pascal case
+       * [file-name] - kebab case
+       * [file_name] - snake case
+       * [FILE_NAME] - constant case
+       */
       fileName: string;
       template: string;
     }>;
   }>;
   modify?: Array<{
+    /**
+     * Relative path to the file (src/directory/file.js).
+     */
     file: string;
     from: string;
     to: string;
