4bbf63386b495c67e49d8a57c788233573de8373
Rokas Anisas <rokas.anisas@gmail.com>
2021-04-08 08:14:40 +0300

    tsconfig init

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,23 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+import webpack from "webpack";
 
-module.exports = {
-  entry: './lib/index.ts',
-  target: 'node',
+const webpackConfig: webpack.Configuration = {
+  entry: "./lib/index.ts",
+  target: "node",
+  mode: "production",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
-  }
-}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: [/node_modules/],
+        use: ["babel-loader"],
+      },
+    ],
+  },
+};
+
+export default webpackConfig;
