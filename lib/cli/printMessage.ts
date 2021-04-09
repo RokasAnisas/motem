d643977f3292a855cf618437034d01bbe3aa3709
Rokas Anisas <rokas.anisas@gmail.com>
2021-04-09 10:15:36 +0300

    add warning label style

--- lib/cli/printMessage.ts
+++ lib/cli/printMessage.ts
@@ -2,13 +2,14 @@ export const printMessage = ({ type, message }: PrintMessageProps): void => {
   const typeMap: { [key in MessageType]: string} = {
     title: '\x1b[4m%s\x1b[0m',
     error: '\x1b[31m%s\x1b[0m',
+    warning: '\x1b[33m%s\x1b[0m',
     success: '\x1b[32m%s\x1b[0m',
   };
 
   console.log(typeMap[type], `${message}`);
 }
 
-type MessageType = 'title' | 'success' | 'error';
+type MessageType = 'title' | 'success' | 'error' | 'warning';
 
 interface PrintMessageProps {
   type: MessageType;
