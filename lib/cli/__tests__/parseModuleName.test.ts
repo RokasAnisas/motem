abca8cad16c5ee1b8e8205f72769d13419972416
Rokas Anisas <rokas.anisas@gmail.com>
2021-04-09 08:52:09 +0300

    update prase name test

--- lib/cli/__tests__/parseModuleName.test.ts
+++ lib/cli/__tests__/parseModuleName.test.ts
@@ -12,6 +12,50 @@ describe("parseModuleName", () => {
     expect(result).toBe('path/to/testName');
   });
 
+  it("Should parse PascalCase module name", () => {
+    const testString = "path/to/[ModuleName]";
+    const testModuleName = "TEST NAME";
+    const result = parseModuleName({
+      moduleName: testModuleName,
+      string: testString,
+    });
+
+    expect(result).toBe('path/to/TestName');
+  });
+
+  it("Should parse kebab-case module name", () => {
+    const testString = "path/to/[module-name]";
+    const testModuleName = "TEST NAME";
+    const result = parseModuleName({
+      moduleName: testModuleName,
+      string: testString,
+    });
+
+    expect(result).toBe('path/to/test-name');
+  });
+
+  it("Should parse snake_case module name", () => {
+    const testString = "path/to/[module_name]";
+    const testModuleName = "TEST NAME";
+    const result = parseModuleName({
+      moduleName: testModuleName,
+      string: testString,
+    });
+
+    expect(result).toBe('path/to/test_name');
+  });
+
+  it("Should parse CONSTANT_NAME module name", () => {
+    const testString = "path/to/[MODULE_NAME]";
+    const testModuleName = "test-name";
+    const result = parseModuleName({
+      moduleName: testModuleName,
+      string: testString,
+    });
+
+    expect(result).toBe('path/to/TEST_NAME');
+  });
+
   it("Should parse multiple [moduleNames] per string", () => {
     const testString = "const [ModuleName]: FC<[ModuleName]Props>";
     const testModuleName = "My new component";
