f80cf02a73e0cafdc64a153edbeba43f110a6d8a
Rokas Anisas <rokas.anisas@gmail.com>
2021-04-09 09:28:53 +0300

    Update generateModule.ts

--- lib/cli/generateModule.ts
+++ lib/cli/generateModule.ts
@@ -1,7 +1,8 @@
-import fse from 'fs-extra';
+import fse from "fs-extra";
 
 import { addFile } from "./addFile";
 import { parseModuleName } from "./parseModuleName";
+import { printMessage } from "./printMessage";
 import { readModule } from "./readModule";
 import { PromptResponse } from "./types/PromptResponse.type";
 
@@ -10,20 +11,21 @@ export const generateModule = ({ moduleName, moduleType }: PromptResponse) => {
 
   // Add files
   moduleContents.add?.forEach((directory) => {
+    const dirNameParsed = parseModuleName({
+      string: `${directory.dirPath}`,
+      moduleName: moduleName,
+    });
+    const dirPath = `${process.cwd()}/${dirNameParsed}`;
+    // Generate directory
+    fse.mkdirSync(dirPath, { recursive: true });
+    printMessage({ type: "title", message: `${dirNameParsed}:` });
+
     directory.files.map((file) => {
-      const dirNameParsed = parseModuleName({
-        string: `${directory.dirPath}`,
-        moduleName: moduleName,
-      });
       const fileNameParsed = parseModuleName({
         string: `${file.fileName}`,
         moduleName: moduleName,
       });
-      const dirPath = `${process.cwd()}/${dirNameParsed}`;
       const filePath = `${process.cwd()}/${dirNameParsed}/${fileNameParsed}`;
-
-      // Generate directory
-      fse.mkdirSync(dirPath, { recursive: true });
 
       addFile({
         filePath: filePath,
