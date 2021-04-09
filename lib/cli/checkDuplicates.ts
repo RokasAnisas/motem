e7e84718ccc5e7a5512f8f3f8487d45366959926
Rokas Anisas <rokas.anisas@gmail.com>
2021-04-09 07:46:34 +0300

    check duplicates

---
+++ lib/cli/checkDuplicates.ts
@@ -0,0 +1,6 @@
+import { PromptResponse } from "./types/PromptResponse.type";
+
+export const checkDuplicates = ({ moduleHead, moduleName }: PromptResponse) => {
+  console.log(moduleHead);
+  console.log(moduleName);
+};
--- lib/cli/index.ts
+++ lib/cli/index.ts
@@ -1,15 +1,13 @@
 #! /usr/bin/env node
+import { checkDuplicates } from "./checkDuplicates";
 import { getFilesList } from "./getFilesList";
-import { generateModule } from "./generateModule";
+// import { generateModule } from "./generateModule";
 import { promptModules } from "./promptModules";
 
 const dirParameter = process.argv.slice(2)[0];
 const directoryPath = `${process.cwd()}/${dirParameter}`;
 
-const modulesList = getFilesList(directoryPath)
+const modulesList = getFilesList(directoryPath);
 
-promptModules(modulesList).then(value => generateModule(value));
-
-// modulesList.forEach((module) => {
-//   compileModule(module);
-// });
+// promptModules(modulesList).then(value => generateModule(value));
+promptModules(modulesList).then((value) => checkDuplicates(value));
